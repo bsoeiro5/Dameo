dad917e9da797143970dd9c82eafcfed5d39882d
Francisco Vilas Boas Machado <franciscovbmachado@gmail.com>
2025-03-20 21:32:17 +0000

    Update constants.py

---
+++ dameo main/dameo sub/constants.py
@@ -0,0 +1,20 @@
+import pygame
+
+# Constantes do tabuleiro
+LARGURA, ALTURA = 800, 800  
+LINHAS, COLUNAS = 8, 8
+TAMANHO_QUADRADO = LARGURA // COLUNAS
+
+# Cores RGB
+VERMELHO = (255, 0, 0)
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+AZUL = (0, 0, 255)
+CINZA = (128, 128, 128)
+AMARELO = (255, 255, 0)
+VERDE = (0, 255, 0)
+ROSA = (255, 0, 255)
+LARANJA = (255, 165, 0)
+AZUL_CLARO = (135, 206, 250)
+CASTANHO = (139, 69, 19)
+ROXO = (128, 0, 128)
