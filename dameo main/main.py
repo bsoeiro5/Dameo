493c246641b247a42efee2be95e3020efa0fde4c
Bernardo Soeiro <bernardo.d.soeiro@gmail.com>
2025-03-21 10:07:28 +0000

    Update main.py

--- dameo main/main.py
+++ dameo main/main.py
@@ -11,6 +11,9 @@ def main():             #função para correr o jogo
     clock = pygame.time.Clock()
     tabuleiro = Tabuleiro()
 
+    peça = tabuleiro.get_peça(0,1)
+    tabuleiro.movimento(peça, 4,3)
+
     while run:
         clock.tick(60)  #60 FPS
         
