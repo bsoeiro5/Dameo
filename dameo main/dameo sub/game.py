3e74d8bf664a9f90231c6b65c10e621cff438089
Alvaro Castro <alvarogil2006@gmail.com>
2025-03-23 01:04:25 +0000

    Update game.py

--- dameo main/dameo sub/game.py
+++ dameo main/dameo sub/game.py
@@ -1,5 +1,5 @@
 import pygame
-from .constants import VERDE, LARANJA
+from .constants import VERDE, LARANJA, TAMANHO_QUADRADO, ALTURA, AZUL
 from .tabuleiro import Tabuleiro
 
 class Game:
@@ -12,9 +12,12 @@ class Game:
         self.tabuleiro = Tabuleiro()
         self.turn = VERDE
         self.valid_moves = {}
+    def winner(self):
+        return self.tabuleiro.winner() 
     
     def update(self):
         self.tabuleiro.desenhar(self.win)
+        self.draw_valid_moves(self.valid_moves)
         pygame.display.update()
 
     def reset(self):
@@ -26,12 +29,12 @@ class Game:
             if not result:
                 self.selected = None
                 self.select(linha,coluna)
-        else:
-            peça = self.tabuleiro.get_peça(linha,coluna)
-            if peça != 0 and peça.cor == self.turn:
-                self.selected = peça
-                self.valid_moves = self.tabuleiro.get_valid_moves(peça)
-                return True
+        
+        peça = self.tabuleiro.get_peça(linha,coluna)
+        if peça != 0 and peça.cor == self.turn:
+            self.selected = peça
+            self.valid_moves = self.tabuleiro.get_valid_moves(peça)
+            return True
         
         return False
 
@@ -39,13 +42,21 @@ class Game:
         peça = self.tabuleiro.get_peça(linha,coluna)
         if self.selected and peça == 0 and (linha,coluna) in self.valid_moves:
             self.tabuleiro.movimento(self.selected,linha,coluna)
+            skipped = self.valid_moves[(linha,coluna)]
+            if skipped:
+                self.tabuleiro.remove(skipped)
             self.change_turn()
         else:
             return False
 
         return True
+    def draw_valid_moves(self, movimentos):
+        for movimento in movimentos:
+            linha, coluna = movimento
+            pygame.draw.circle(self.win, AZUL, (coluna * TAMANHO_QUADRADO + TAMANHO_QUADRADO//2, linha * TAMANHO_QUADRADO + TAMANHO_QUADRADO//2), 15)
     
     def change_turn(self):
+        self.valid_moves = {}
         if self.turn == VERDE:
             self.turn = LARANJA
         else:
