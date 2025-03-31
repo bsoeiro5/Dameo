146ba19d7b421dfcc190d0dcc31c8ce509a12bc5
Francisco Vilas Boas Machado <franciscovbmachado@gmail.com>
2025-03-31 15:28:14 +0100

    Create algoritmo.py

---
+++ dameo main/minimax/algoritmo.py
@@ -0,0 +1,44 @@
+from copy import deepcopy
+import pygame
+from dameo_sub.constants import LARANJA, VERDE
+
+def minimax(tabuleiro, profundidade, max_player, game):
+    if profundidade == 0 or tabuleiro.winner() != None:
+        return tabuleiro.evaluate(), tabuleiro
+
+    if max_player:
+        maxEval = float('-inf')
+        best_move = None
+        for move in get_all_moves(tabuleiro, game, VERDE):
+            evaluation = minimax(move, profundidade - 1, False, game)[0]
+            maxEval = max(maxEval, evaluation)
+            if maxEval == evaluation:
+                best_move = move
+        return maxEval, best_move
+    else:
+        minEval = float('inf')
+        best_move = None
+        for move in get_all_moves(tabuleiro, game, LARANJA):
+            evaluation = minimax(move, profundidade - 1, True, game)[0]
+            minEval = min(minEval, evaluation)
+            if minEval == evaluation:
+                best_move = move
+        return minEval, best_move
+    
+def simular_movimento(peça, movimento, tabuleiro, game, skip):
+    tabuleiro.movimento(peça, movimento[0], movimento[1])
+    if skip:
+        tabuleiro.remove(skip)
+    return tabuleiro
+
+def get_all_moves(tabuleiro, cor, game):
+    moves = []
+
+    for piece in tabuleiro.get_all_peças(cor):
+        valid_moves = tabuleiro.get_valid_moves(piece)
+        for move, skip in valid_move.items():
+            temp_tabuleiro = deepcopy(tabuleiro)
+            new_tabuleiro = simular_movimento(piece, move, temp_tabuleiro, game, skip)
+            moves.append(new_tabuleiro)
+
+    return moves
