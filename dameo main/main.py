98fae50e38f87a3e3366393b977f81645604a0da
Bernardo Soeiro <bernardo.d.soeiro@gmail.com>
2025-03-31 17:46:31 +0100

    Update main.py

--- dameo main/main.py
+++ dameo main/main.py
@@ -34,17 +34,16 @@ def jogo_principal(modo):
 
             elif modo == "pvc":  # Player vs Computer
                 if game.turn == VERDE:  # Jogador Verde joga manualmente
-                    value, new_tabuleiro = minimax(game.tabuleiro, 3, True, game)
-                    game.ai_move(new_tabuleiro)
                     if event.type == pygame.MOUSEBUTTONDOWN:
                         x, y = pygame.mouse.get_pos()
                         linha = y // 100
                         coluna = x // 100
                         game.select(linha, coluna)
-                        
+
                 else:  # IA (Laranja) joga automaticamente
-                    pygame.time.delay(500)
+                    value, new_tabuleiro = minimax(game.tabuleiro, 3, True, game)
                     game.ai_move(game.tabuleiro)
+                    pygame.time.delay(500)
 
             elif modo == "cvc":  # Computer vs Computer
                 pygame.time.delay(500)  # Pequena pausa para visualizar as jogadas
