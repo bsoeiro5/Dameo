36d0a4c5ff845beb442ae7906796dfa48fdb8f58
Francisco Vilas Boas Machado <franciscovbmachado@gmail.com>
2025-04-11 02:47:53 +0100

    Update peças.py

--- dameo main/dameo sub/peças.py
+++ dameo main/dameo sub/peças.py
@@ -1,14 +1,16 @@
-from .constants import LARANJA, VERDE, TAMANHO_QUADRADO, LARANJA_ESCURO, PRETO, VERDE_ESCURO, coroa
+from .constants import LARANJA, VERDE, LARANJA_ESCURO, PRETO, VERDE_ESCURO, coroa
 import pygame
 
 class Peças:
     ESPAÇO = 10
     BORDA = 3
 
-    def __init__(self, linha, coluna, cor):
+    def __init__(self, linha, coluna, cor, tamanho_quadrado):
         self.linha = linha
         self.coluna = coluna
         self.cor = cor
+        self.TAMANHO_QUADRADO = tamanho_quadrado
+        
         if self.cor == VERDE:
             self.corsecundaria = VERDE_ESCURO
         else:
@@ -25,14 +27,14 @@ class Peças:
         self.calc_pos()
 
     def calc_pos(self):     #calcular a posição da peça
-        self.x = TAMANHO_QUADRADO * self.coluna + TAMANHO_QUADRADO // 2
-        self.y = TAMANHO_QUADRADO * self.linha + TAMANHO_QUADRADO // 2
+        self.x = self.TAMANHO_QUADRADO * self.coluna + self.TAMANHO_QUADRADO // 2
+        self.y = self.TAMANHO_QUADRADO * self.linha + self.TAMANHO_QUADRADO // 2
 
     def make_king(self):
         self.king = True
 
     def draw(self, win):
-        raio = TAMANHO_QUADRADO // 2 - self.ESPAÇO
+        raio = self.TAMANHO_QUADRADO // 2 - self.ESPAÇO
         pygame.draw.circle(win, self.corsecundaria, (self.x, self.y), raio + self.BORDA)
         pygame.draw.circle(win, self.cor, (self.x, self.y), raio)
         pygame.draw.circle(win, self.corsecundaria, (self.x, self.y), raio - 5)
@@ -40,13 +42,9 @@ class Peças:
         if self.king:
             win.blit(coroa, (self.x - coroa.get_width() // 2, self.y - coroa.get_height() // 2))
     
-    def movimento(self,linha,coluna):
+    def movimento(self, linha, coluna):
         self.linha = linha
         self.coluna = coluna
-        self.calc_pos()
-    
-    def __repr__(self):
-        return str(self.cor)
         self.calc_pos()
     
     def __repr__(self):
